a880184652e141aa9adca84c5dc23452158dea69
mohammad javad ghasemy <wowkhanmoghol66@yahoo.com>
2019-05-03 05:46:49 +0430

    config socket.io

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const mongo     = require('mongoose');
-const io        = require('socket.io');
-const db_set    = require('./db/set');
-const db_get    = require('./db/get');
+const app    = require('express')();
+const http   = require('http').Server(app);
+const mongo  = require('mongoose');
+const io     = require('socket.io')(http);
+const db_set = require('./db/set');
+const db_get = require('./db/module');
 
 
 mongo.connect('mongodb://127.0.0.1:27017/chat', {useNewUrlParser: true});
@@ -10,3 +12,11 @@ mongo.connect('mongodb://127.0.0.1:27017/chat', {useNewUrlParser: true});
 // db_set.add_user("javad", "geeksesi", "javadkhof", "test", res => {console.log(res)});
 // db_set.add_message("5cc8aae7fae56d27ae1c85cd", "hello world", res => {console.log(res)});
 
+io.on('connection', socket => {
+    console.log("hellow");
+});
+
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+  });
